2893339b1e22d599c68ac485851bb2a5e47a9d71
RoliJ <r.joveini@gmail.com>
2023-07-19 22:22:59 +0330

    authcontrollertest has been added

---
+++ tests/Feature/AuthControllerTest.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Laravel\Passport\Passport;
+use Tests\TestCase;
+
+class AuthControllerTest extends TestCase
+{
+    use RefreshDatabase;
+    use WithFaker;
+
+    /**
+     * Test user registration.
+     *
+     * @return void
+     */
+    public function testUserRegistration()
+    {
+        $userData = [
+            'name' => $this->faker->name,
+            'email' => $this->faker->unique()->safeEmail,
+            'password' => 'password',
+            'password_confirmation' => 'password',
+        ];
+        $response = $this->postJson('api/register', $userData);
+        dd($response);
+        $response->assertStatus(201)
+            ->assertJsonStructure(['token']);
+    }
+
+    /**
+     * Test user login.
+     *
+     * @return void
+     */
+    public function testUserLogin()
+    {
+        $user = User::factory()->create(['password' => bcrypt('password')]);
+
+        $loginData = [
+            'email' => $user->email,
+            'password' => 'password',
+        ];
+
+        $response = $this->postJson('api/login', $loginData);
+
+        $response->assertStatus(200)
+            ->assertJsonStructure(['token']);
+    }
+
+    /**
+     * Test user logout.
+     *
+     * @return void
+     */
+    public function testUserLogout()
+    {
+        $user = User::factory()->create();
+
+        Passport::actingAs($user);
+
+        $response = $this->postJson('api/logout');
+
+        $response->assertStatus(200)
+            ->assertJson(['message' => 'Logged out successfully']);
+    }
+}
