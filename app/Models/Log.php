f4a1b7ec3fbc979a943c4d2dc4614618bbc30e9d
RoliJ <r.joveini@gmail.com>
2023-07-19 13:16:51 +0330

    mysql db engine is defined in the config file + log model has been added

---
+++ app/Models/Log.php
@@ -0,0 +1,140 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+/**
+ * Log Model representing a log entry from a microservice.
+ *
+ * This model provides access to the logs table in the database.
+ */
+class Log extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'logs';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'service_name',
+        'logged_at',
+        'method',
+        'endpoint',
+        'protocol',
+        'status',
+    ];
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = [
+        'logged_at',
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
+
+    /**
+     * Retrieve the formatted logged_at attribute.
+     *
+     * @param  string  $value
+     * @return string
+     */
+    public function getLoggedAtAttribute($value)
+    {
+        // Format the logged_at attribute as desired (e.g., to a specific date and time format)
+        return $this->asDateTime($value)->format('Y-m-d H:i:s');
+    }
+
+    /**
+     * Scope a query to filter logs by service name.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @param  string  $serviceName
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeByServiceName($query, $serviceName)
+    {
+        // Apply a filter to retrieve logs by service name
+        return $query->where('service_name', $serviceName);
+    }
+
+    /**
+     * Scope a query to filter logs by logged_at.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @param  string  $loggedAt
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeByLoggedAt($query, $loggedAt)
+    {
+        // Apply a filter to retrieve logs by logged_at
+        return $query->where('logged_at', $loggedAt);
+    }
+
+    /**
+     * Scope a query to filter logs by method.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @param  string  $method
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeByMethod($query, $method)
+    {
+        // Apply a filter to retrieve logs by method
+        return $query->where('method', $method);
+    }
+
+    /**
+     * Scope a query to filter logs by endpoint.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @param  string  $endpoint
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeByEndpoint($query, $endpoint)
+    {
+        // Apply a filter to retrieve logs by endpoint
+        return $query->where('endpoint', $endpoint);
+    }
+
+    /**
+     * Scope a query to filter logs by protocol.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @param  string  $protocol
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeByProtocol($query, $protocol)
+    {
+        // Apply a filter to retrieve logs by protocol
+        return $query->where('protocol', $protocol);
+    }
+    
+    /**
+     * Scope a query to filter logs by status code.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @param  int  $statusCode
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeByStatusCode($query, $statusCode)
+    {
+        // Apply a filter to retrieve logs by status code
+        return $query->where('status', $statusCode);
+    }
+}
